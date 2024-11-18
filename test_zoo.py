4c0272d2e8d119e68a43055f1e68b0c2dc7aff95
BIllCIfhEr <bill26122546rock26@gmail.com>
2024-11-18 17:56:19 +0000

    don't read me

--- test_zoo.py
+++ test_zoo.py
@@ -6,7 +6,19 @@ class TestZoo(unittest.TestCase):
         self.zoo = Zoo()
 
     def test_child_ticket_price(self):
-        self.assertEqual(self.zoo.get_ticket_price(5), 50)
+        self.assertEqual(self.zoo.get_ticket_price(-999), "why you do this")
+
+    def test_child_ticket_price1(self):
+        self.assertEqual(self.zoo.get_ticket_price(7), 50)
+
+    def test_child_ticket_price2(self):
+        self.assertEqual(self.zoo.get_ticket_price(14), 100)
+
+    def test_child_ticket_price3(self):
+        self.assertEqual(self.zoo.get_ticket_price(49), 150)
+
+    def test_child_ticket_price4(self):
+        self.assertEqual(self.zoo.get_ticket_price(69), 100)
        
     # Add your additional test cases here.
 
